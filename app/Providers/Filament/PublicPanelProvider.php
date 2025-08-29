c9d78675c48f41553ed403f81796a2b8bc1ad9b6
Schilly <patrick.schillemat@gmail.com>
2025-08-29 15:50:26 -0400

    composer updates

--- app/Providers/Filament/PublicPanelProvider.php
+++ app/Providers/Filament/PublicPanelProvider.php
@@ -8,15 +8,12 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
-use Illuminate\Support\Str;
-use Pschilly\FilamentDcsServerStats\Widgets\DailyPlayersChart;
 
 class PublicPanelProvider extends PanelProvider
 {
