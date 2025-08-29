fff525f64ab5e5a9d562cd43edc20e590b41a5f8
Schilly <patrick.schillemat@gmail.com>
2025-08-29 17:26:03 -0400

    remove telescope & socialite

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\PublicPanelProvider::class,
-    App\Providers\Filament\StatsConfigPanelProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+    App\Providers\Filament\StatsConfigPanelProvider::class
 ];
