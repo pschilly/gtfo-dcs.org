682c9bc933f18c0f68030a1cbd4be7d9aee9ded3
Schilly <patrick.schillemat@gmail.com>
2025-08-29 17:26:23 -0400

    seeder update for default user & settings

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Inerba\DbConfig\DbConfig;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +14,101 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
+        // Default Admin user
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@changeme.com',
+            'password' => bcrypt('password')
         ]);
+
+        // Default Settings
+        DbConfig::set('brand.site-name', 'DCS Statistics Dashboard');
+        DbConfig::set('brand.brand-logo', NULL);
+        DbConfig::set('brand.favicon', NULL);
+        DbConfig::set('brand.header-image', 'dcs-header-image.jpg');
+        DbConfig::set(
+            'brand.colors',
+            [[
+                'primary' => 'emerald',
+                'gray' => 'zinc',
+                'success' => 'green',
+                'info' => 'sky',
+                'warning' => 'amber',
+                'danger' => 'red'
+            ]]
+        );
+
+        DbConfig::set('feature.server-selector', TRUE);
+        DbConfig::set(
+            'feature.pages',
+            [
+                [
+                    'page-name' => 'leaderboard'
+                ],
+                [
+                    'page-name' => 'player-stats'
+                ],
+                [
+                    'page-name' => 'squadrons'
+                ],
+                [
+                    'page-name' => 'servers'
+                ]
+            ]
+        );
+        DbConfig::set(
+            'feature.dashboard-widgets',
+            [
+                [
+                    'widget-name' => 'server-statistics'
+                ],
+                [
+                    'widget-name' => 'daily-players-chart'
+                ],
+                [
+                    'widget-name' => 'top-pilots'
+                ],
+                [
+                    'widget-name' => 'top-squadrons'
+                ]
+            ]
+        );
+        DbConfig::set(
+            'feature.player-stats-widgets',
+            [
+                [
+                    'widget-name' => 'pve-chart'
+                ],
+                [
+                    'widget-name' => 'pvp-chart'
+                ],
+                [
+                    'widget-name' => 'module-chart'
+                ],
+                [
+                    'widget-name' => 'sortie-chart'
+                ],
+                [
+                    'widget-name' => 'combat-chart'
+                ]
+            ]
+        );
+        DbConfig::set(
+            'feature.leaderboard-columns',
+            [
+                [
+                    'column-name' => 'deaths'
+                ],
+                [
+                    'column-name' => 'kdr'
+                ],
+                [
+                    'column-name' => 'credits'
+                ],
+                [
+                    'column-name' => 'playtime'
+                ]
+            ]
+        );
     }
 }
