e4fbbe8367e21154a905fd0d639831c047365f61
Schilly <patrick.schillemat@gmail.com>
2025-09-02 15:44:28 -0400

    header ssl vs non-ssl fix

--- resources/views/filament/header-bar.blade.php
+++ resources/views/filament/header-bar.blade.php
@@ -1,1 +1,8 @@
-<div class="bg-cover bg-center bg-no-repeat bg-blend-multiply h-48 bg-gray-600" style="background-image: url('{{ secure_asset('/storage/'.\Inerba\DbConfig\DbConfig::get('brand.header-image', ''))}}');"></div>
+@php
+    $headerImage = \Inerba\DbConfig\DbConfig::get('brand.header-image', '');
+    $imageUrl = empty($headerImage)
+        ? ''
+        : (request()->isSecure() ? secure_asset('/storage/' . $headerImage) : asset('/storage/' . $headerImage));
+@endphp
+
+<div class="bg-cover bg-center bg-no-repeat bg-blend-multiply h-48 bg-gray-600" style="background-image: url('{{ $imageUrl }}');"></div>
